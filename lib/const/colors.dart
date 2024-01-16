238baaffe07b61887e833479352cb9c594ec8e6c
ahmad-mukhlish <ahmad.mukhlish.saputra@gmail.com>
2024-01-16 13:11:17 +0700

    add static chat page

---
+++ lib/const/colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+const primaryDark = Color.fromRGBO(3, 26, 48, 1);
+const darkAccent = Color.fromRGBO(23, 22, 47, 1);
+const lightBGHome = Color.fromRGBO(233, 229, 223, 1);
+const lightThemeBlue = Color.fromRGBO(10, 102, 194, 1);
---
+++ lib/entity/person_chat.dart
@@ -0,0 +1,15 @@
+class PersonChat {
+  final String name;
+  final String message;
+  final String time;
+  final int totalUnread;
+  final String image;
+
+  PersonChat( {
+    required this.name,
+    required this.message,
+    required this.time,
+    required this.totalUnread,
+    required this.image,
+  });
+}
